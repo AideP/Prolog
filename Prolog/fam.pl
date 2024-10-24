e72ffd330c5f579eb0f35a6fd24caaf9586ef7d5
Aide Perez <perezaide108@gmail.com>
2024-10-23 23:57:02 -0600

    Comienzo pruebas de arbol genealogico solo padres, abuelos, tios y primos

--- Prolog/fam.pl
+++ Prolog/fam.pl
@@ -16,4 +16,3 @@ padrede(pedro, lea).
 padrede(pedro, josefino).
 madrede(filomena, pedro).
 abuelos(X,Y):-padrede(X,Z),padrede(Z,Y); madrede(X,Z),padrede(Z,Y).
-hermano(X,Y):- padrede(P,X), padrede(P,Y); madrede(M,X), madrede(M,Y).
---
+++ Prolog/mifam.pl
@@ -0,0 +1,49 @@
+
+
+padrede(raul, aide).
+padrede(raul, cristina).
+padrede(raul, raul_hijo).
+
+madrede(maricruz, aide).
+madrede(maricruz, cristina).
+madrede(maricruz, raul_hijo).
+
+
+padrede(benjamin, raul).
+madrede(cristina_abuela, raul).
+madrede(teresa, maricruz).
+
+hermano(jose, maricruz).
+hermana(griselda, maricruz).
+
+hermano(eduardo, raul).
+hermana(leticia, raul).
+hermana(edith, raul).
+hermano(jorge, raul).
+hermano(javier, raul).
+
+esposo(alejandroM, edith).
+esposo(jorge, teresa_esposa).
+esposa(edithM, alejandro).
+
+padrede(jorge, esmeralda).
+padrede(jorge, hugo).
+padrede(jorge, nataly).
+madrede(teresa_esposa, esmeralda).
+madrede(teresa_esposa, hugo).
+madrede(teresa_esposa, nataly).
+
+padrede(alejandroM, alejandro_hijo).
+padrede(alejandroM, alejandra).
+madrede(edithM, alejandro_hijo).
+madrede(edithM, alejandra).
+
+
+madrede(leticia, emely).
+
+padrede(alejandro, alejandrito).
+padrede(alejandro, eleazar).
+padrede(alejandro, kenia).
+madrede(edith, alejandrito).
+madrede(edith, eleazar).
+madrede(edith, kenia).
