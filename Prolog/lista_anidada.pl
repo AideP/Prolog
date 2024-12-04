0c256b798eabc23c4425c252184e4697aaab5092
Aide Perez <perezaide108@gmail.com>
2024-12-04 09:50:23 -0600

    prueba de entrada de datos para lista_anidada.pl

--- Prolog/lista_anidada.pl
+++ Prolog/lista_anidada.pl
@@ -1,1 +1,5 @@
-flatten([1, [2, [3, 4], 5], [6]], R).
+start :-
+    write('Ingrese una lista para aplanar (ejemplo: [[a, [b, c]]): '),
+    read(UserList),
+    write(UserList).
+%flatten([1, [2, [3, 4], 5], [6]], R).
