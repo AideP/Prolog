4ad620d68bac8054896091801fed97e82ca337c6
Aide Perez <perezaide108@gmail.com>
2024-12-04 12:46:08 -0600

    intento para el problema 2 permutación, aun no esta terminado

--- Prolog/permutaciones.pl
+++ Prolog/permutaciones.pl
@@ -1,3 +1,10 @@
+permutacion :-
+    write('Ingrese una lista para obtener sus permutaciones (ejemplo: [a, b, c]): '),
+    read(UserList),
+
+
+
+    
 permutacion([], []).
 permutacion([H|T], P) :-
     permutacion(T, R),
