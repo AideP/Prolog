d53849b66460ca957f06ecde6d7acd598c6a5ff5
Aide Perez <perezaide108@gmail.com>
2024-12-05 09:31:25 -0600

    Permutaciones.pl completo

--- Prolog/permutaciones.pl
+++ Prolog/permutaciones.pl
@@ -1,14 +1,11 @@
 permutacion :-
     write('Ingrese una lista para obtener sus permutaciones (ejemplo: [a, b, c]): '),
-    read(UserList),
-    permutar(List, P),
+    read(UserList), 
+    findall(P, permutar(UserList, P), Permutations), %findall para encontrar todas las permutaciones posibles
     write('Las permutaciones son: '), nl,
-    write(Permutations), nl
+    write(Permutations), nl.
 
-permutacion([], []).
-permutacion([H|T], P) :-
-    permutacion(T, R),
-    insertar(H, R, P).
-
-insertar(X, L, [X|L]).
-insertar(X, [H|T], [H|R]) :- insertar(X, T, R).
+permutar([], []). 
+permutar([H|T], P) :- permutar(T, R), insertar(H, R, P). 
+insertar(X, L, [X|L]). 
+insertar(X, [H|T], [H|R]) :- insertar(X, T, R). 
