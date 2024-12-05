37ab45aba14bfe3aa69a384091f7bc8aafb25baa
Aide Perez <perezaide108@gmail.com>
2024-12-05 09:19:01 -0600

    modificacion en permutaciones, se le intenta dar formato, aun no sirve

--- Prolog/permutaciones.pl
+++ Prolog/permutaciones.pl
@@ -1,10 +1,10 @@
 permutacion :-
     write('Ingrese una lista para obtener sus permutaciones (ejemplo: [a, b, c]): '),
     read(UserList),
+    permutar(List, P),
+    write('Las permutaciones son: '), nl,
+    write(Permutations), nl
 
-
-
-    
 permutacion([], []).
 permutacion([H|T], P) :-
     permutacion(T, R),
